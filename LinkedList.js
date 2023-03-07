c7808e1e8f0cccb746b634fc226b4f87cddbe08b
AndonG7 <gjorgjiev.andon@uklo.edu.mk>
2023-03-08 00:10:36 +0100

    finished

--- LinkedList.js
+++ LinkedList.js
@@ -12,5 +12,165 @@ export class LinkedList {
         this.size++;
     }
 
-    
+    //Add last node
+    append(value) {
+        let node = new Node(value);
+        let temp;
+
+        //If list is empty, the new node is the head node
+        if(this.head === null) {
+            this.head = node;
+        } 
+        //Otherwise make the temporary variable be the head value
+         else {
+            temp = this.head;
+            
+            //Then cycle through all the remaining nodes until the last one
+            while (temp.next) {
+                temp = temp.next;
+            }
+
+            //The last node is given to the temp variable and 
+            //append the new node next to the last node 
+            temp.next = node;
+
+            //Increment the size value
+            this.size++;
+        }
+
+    }
+
+    //Get the size of the linked list
+    getSize() {
+        return this.size;
+    }
+
+    //Get the head node of the linked list
+    getHead() {
+        return this.head;
+    }
+
+    //Get the last  node of the linked list
+    //Same principle applies as the add Last Node without the adding part
+    getTail() {
+        let temp;
+
+        if(this.head === null) {
+            return null;
+        } 
+         else {
+            temp = this.head;
+            
+            while (temp.next) {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+
+    //Get element at index
+    atIndex(index) {
+        let temp = this.head;
+
+        if (index === 0) {
+            return temp;
+        } else {
+            for (let i=0; i<index; i++){
+                temp = temp.next;
+            }
+        }
+        return temp;
+    }
+
+    //Remove last node from the list
+    //Same principle applies as the getLast function
+    pop() {
+        let temp;
+
+        if(this.head === null) {
+            return null;
+        } 
+         else {
+            temp = this.head;
+            
+            //Get the before the last element
+            for (let i=0; i<this.size-2; i++){
+                temp = temp.next;
+            }
+            //Make it's next element null
+            temp.next = null;
+        }
+            //Decrement the size value
+            this.size--;
+
+    }
+
+    //Check if a value is contained in the list. If it is return true.
+    contains(value) {
+        let temp;
+
+        if(this.head === null) {
+            return false;
+        } 
+         else {
+            temp = this.head;
+            
+            for (let i=0; i<this.size; i++) {
+                console.log(temp.data);
+                if (temp.data === value) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+
+        }
+
+        return false;
+    }
+
+    //Get element index if it is contained in the list
+    find(value) {
+        let temp;
+
+        if(this.head === null) {
+            return null;
+        } 
+         else {
+            temp = this.head;
+            
+            for (let i=0; i<this.size; i++) {
+                console.log(temp.data);
+                if (temp.data === value) {
+                    return i;
+                }
+                temp = temp.next;
+            }
+
+        }
+
+        return null;
+    }
+
+    //Print elements as string
+    toString() {
+        let temp;
+        let nodeStr = "";
+
+        if(this.head === null) {
+            return false;
+        } 
+         else {
+            temp = this.head;
+            
+            for (let i=0; i<this.size; i++) {
+                nodeStr = nodeStr + "( " + temp.data + " ) " + "-> "; 
+                temp = temp.next;
+            }
+
+        }
+
+        nodeStr = nodeStr + "null";
+
+        return nodeStr;
+    }
 }
--- main.js
+++ main.js
@@ -5,7 +5,15 @@ const ll1 = new LinkedList();
 
 ll1.prepend(10);
 ll1.prepend(20);
-ll1.prepend(30);
+ll1.append(40);
+ll1.append(50);
+ll1.pop();
 
-
+// console.log(ll1.getSize());
+// console.log(ll1.getHead());
+// console.log(ll1.getTail());
+// console.log(ll1.atIndex(0));
+// console.log(ll1.contains(50));
+// console.log(ll1.find(40));
+console.log(ll1.toString());
 console.log(ll1);
